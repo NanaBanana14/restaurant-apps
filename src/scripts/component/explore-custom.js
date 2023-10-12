d327543e283ac4a99fecd0a6dfddcf6a82265814
Ika Triyana <134359016+NanaBanana14@users.noreply.github.com>
2023-10-12 16:11:34 +0700

    Update to lit element

--- src/scripts/component/explore-custom.js
+++ src/scripts/component/explore-custom.js
@@ -1,105 +1,90 @@
-class ExploreComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.shadowDOM = this.attachShadow({ mode: 'open' });
-  }
+import { LitElement, html, css } from 'lit-element';
 
-  connectedCallback() {
-    this.render();
-  }
+class ExploreComponent extends LitElement {
+  static styles = css`
+    .section-title {
+      margin: 20px;
+      text-align: center;
+      color: #C63D2F;
+      font-size: 20px;
+    }
+    .explore {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+      gap: 20px;
+      align-items: flex-start;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.2);
+    }
+    .headline {
+      display: grid;
+      grid-template-columns: 1fr;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.2);
+      transition: transform 0.3s, background-color 0.3s, color 0.3s;
+      cursor: pointer;
+    }
+    .headline:hover {
+      transform: translateY(-10px);
+      background-color: #FF9B50;
+      color: white;
+    }
+    .headline__figure {
+      width: 100%;
+      position: relative;
+    }
+    .headline__figure img {
+      width: 100%;
+      height: auto;
+      border-radius: 5px 5px 0 0;
+    }
+    .headline__figure figcaption {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      background-color: #C63D2F;
+      color: white;
+      font-size: 13px;
+      padding: 8px 0;
+    }
+    .headline__content {
+      padding: 20px;
+    }
+    .headline__title {
+      font-size: 24px;
+      color: #333;
+      margin-bottom: 10px;
+    }
+    .headline__description {
+      font-size: 16px;
+      color: #666;
+      margin-bottom: 20px;
+    }
+    .headline__button {
+      text-transform: uppercase;
+      margin-top: 24px;
+      cursor: pointer;
+      padding: 20px;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      background-color: #C63D2F;
+      transition: opacity 0.3s;
+    }
+    .headline__button:hover {
+      opacity: 0.8;
+    }
+  `;
 
+  // eslint-disable-next-line class-methods-use-this
   render() {
-    this.shadowDOM.innerHTML = `
-      <style>
-        .section-title {
-          margin: 20px;
-          text-align: center;
-          color: #C63D2F;
-          font-size: 20px;
-        }
-        .explore {
-          display: grid;
-          grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-          gap: 20px;
-          align-items: flex-start;
-          padding: 20px;
-          border-radius: 5px;
-          box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.2);
-        }
-
-        .headline {
-          display: grid;
-          grid-template-columns: 1fr;
-          padding: 20px;
-          border-radius: 5px;
-          box-shadow: 0 2px 8px 2px rgba(0, 0, 0, 0.2);
-          transition: transform 0.3s, background-color 0.3s, color 0.3s;
-          cursor: pointer;
-        }
-
-        .headline:hover {
-          transform: translateY(-10px);
-          background-color: #FF9B50;
-          color: white;
-        }
-
-        .headline__figure {
-          width: 100%;
-          position: relative;
-        }
-
-        .headline__figure img {
-          width: 100%;
-          height: auto;
-          border-radius: 5px 5px 0 0;
-        }
-
-        .headline__figure figcaption {
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          background-color: #C63D2F;
-          color: white;
-          font-size: 13px;
-          padding: 8px 0;
-        }
-
-        .headline__content {
-          padding: 20px;
-        }
-
-        .headline__title {
-          font-size: 24px;
-          color: #333;
-          margin-bottom: 10px;
-        }
-
-        .headline__description {
-          font-size: 16px;
-          color: #666;
-          margin-bottom: 20px;
-        }
-
-        .headline__button {
-          text-transform: uppercase;
-          margin-top: 24px;
-          cursor: pointer;
-          padding: 20px;
-          color: white;
-          border: none;
-          border-radius: 5px;
-          background-color: #C63D2F;
-          transition: opacity 0.3s;
-        }
-
-        .headline__button:hover {
-          opacity: 0.8;
-        }
-      </style>
+    return html`
       <div class="section-title">
-        <h1>Apa Yang Bisa Kamu Temukan Disini ?</h1>
+        <h2>Apa Yang Bisa Kamu Temukan Disini ?</h2>
       </div>
       <div class="explore">
         <article class="headline">
@@ -109,7 +94,7 @@ class ExploreComponent extends HTMLElement {
             <figcaption>cooked-roasted-chicken-legs</figcaption>
           </div>
           <div class="headline__content">
-            <h1 class="headline__title">Berbagai Macam Menu ada disini</h1>
+            <h3 class="headline__title">Berbagai Macam Menu ada disini</h3>
             <p class="headline__description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eum
               facere nostrum officiis qui quidem ratione similique, soluta veniam
@@ -127,8 +112,8 @@ class ExploreComponent extends HTMLElement {
               alt="Restaurant" />
             <figcaption>Restaurant</figcaption>
           </div>
-          <div class="headline__content">
-            <h1 class="headline__title">Mau pilih berdasarkan Restoran nya ?</h1>
+          <div class "headline__content">
+            <h3 class="headline__title">Mau pilih berdasarkan Restoran nya ?</h3>
             <p class="headline__description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eum
               facere nostrum officiis qui quidem ratione similique, soluta veniam
@@ -145,7 +130,7 @@ class ExploreComponent extends HTMLElement {
             <figcaption>Resep Makanan</figcaption>
           </div>
           <div class="headline__content">
-            <h1 class="headline__title">Ingin tahu Resep dari suatu Menu ?</h1>
+            <h3 class="headline__title">Ingin tahu Resep dari suatu Menu ?</h3>
             <p class="headline__description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eum
               facere nostrum officiis qui quidem ratione similique, soluta veniam
@@ -162,7 +147,7 @@ class ExploreComponent extends HTMLElement {
             <figcaption>Blog Kuliner</figcaption>
           </div>
           <div class="headline__content">
-            <h1 class="headline__title">Update seputar kuliner ada disini</h1>
+            <h3 class="headline__title">Update seputar kuliner ada disini</h3>
             <p class="headline__description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eum
               facere nostrum officiis qui quidem ratione similique, soluta veniam
@@ -171,8 +156,8 @@ class ExploreComponent extends HTMLElement {
             </p>
             <button class="headline__button">Read More</button>
           </div>
-        </article>
-      </div>
+      </article>
+    </div>
     `;
   }
 }
